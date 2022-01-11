87c9767c726cb9e4ae3e145265dfbb1f6eab56fb
Eduardo Weidman Barijan <eduwb88@gmail.com>
2022-01-11 15:22:51 -0300

    add Board api controller

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\BoardApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['namespace' => 'Api'], function () {
+    Route::get('/board', [BoardApiController::class, 'show']);
 });
