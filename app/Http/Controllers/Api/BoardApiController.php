f9b85bfe6a50f4d22c9c412088b695221d362239
Eduardo Weidman Barijan <eduwb88@gmail.com>
2022-01-11 15:43:49 -0300

    create a new fixed board each time

--- app/Http/Controllers/Api/BoardApiController.php
+++ app/Http/Controllers/Api/BoardApiController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class BoardApiController extends Controller
 {
-    public function show(): void
+    public function show(): JsonResponse
     {
-        var_dump('hello board!');
+        // setups a 2 x 2 board each time
+        $board = [
+            [0,0],
+            [0,0]
+        ];
+
+        return response()->json($board);
     }
 }
